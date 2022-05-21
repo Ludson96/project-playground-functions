a3f2de074e74ec9b0eeead8708bbc6306c8d8a43
Ludson <ludson_ps27@hotmail.com>
2022-05-21 12:21:37 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,29 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array.length !== 11) {
+      return 'Array com tamanho incorreto.';
+    } else if (array[index] < 0 || array[index] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    var repitido =[];
+    for (let index = 0; index < array.length; index += 1) {
+      repitido = array[index];
+      var contador = 0;
+      for (let index = 0; index < array.length; index += 1) {
+        if (repitido === array[index]) {
+        contador += 1;
+        }
+      }
+      if (contador >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      
+    }
+}
+    for (index = 0; index < array.length; index +=1) {
+      return '(' + array[0] + array[1] + ')' + ' ' + array[2] + array[3] + array[4] + array[5] + array[6] + '-' + array[7] + array[8] + array[9] + array[10];
+    }
 }
 
 // Desafio 12
